60c79776b7a178546f946b5f7ec19b392ee8c60c
Autummm <Autummm@163.com>
2021-01-18 18:02:14 +0800

    version 20210118

--- src/test/java/com/autummm/service/AreaServiceTest.java
+++ src/test/java/com/autummm/service/AreaServiceTest.java
@@ -1,7 +1,20 @@
 package com.autummm.service;
 
 import com.autummm.dao.BaseTest;
+import com.autummm.entity.Area;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class AreaServiceTest extends BaseTest {
-
+    @Autowired
+    private AreaService areaService;
+    @Test
+    public void testGetAreaList(){
+        List<Area> areaList = areaService.getAreaList();
+        assertEquals("西苑",areaList.get(0).getAreaName());
+    }
 }
